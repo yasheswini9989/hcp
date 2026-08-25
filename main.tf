d9e7f88fadf400edf4e14801abea9a17f7194234
yasheswini9989 <yasheswini41@gmail.com>
2026-08-25 11:23:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count = 4
 
   ami           = "ami-0e5497a77ef21b5ac"
-  instance_type = "c7i-flex-large"
+  instance_type = "c7i-flex.large"
   key_name      = "server-1"
 
   vpc_security_group_ids = ["sg-0e5fc0d70bedf6283"]
