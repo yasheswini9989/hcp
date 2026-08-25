4a2a3024f01d72e1129e014ba9f29dab449a5976
yasheswini9989 <yasheswini41@gmail.com>
2026-08-25 11:05:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "one" {
+  count = 4
+
+  ami           = "ami-0e5497a77ef21b5ac"
+  instance_type = "c7i-flex-large"
+  key_name      = "server-1"
+
+  vpc_security_group_ids = ["sg-0e5fc0d70bedf6283"]
+
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+variable "instance_names" {
+  default = [
+    "jenkins",
+    "APPSERVER-1",
+    "APPSERVER-2",
+    "Monitoring server"
+  ]
+}
